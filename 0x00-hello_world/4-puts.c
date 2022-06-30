624eb6792ba73205284ffeef6d3e487dc3d189ab
owolabi olatunji <oluwaferanmiadetomiwa7@gmail.com>
2022-06-30 21:13:10 +0100

    low level languague

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+/**
+*main - write '"Programming is like building a multilingual puzzle'
+*Return: Always 0 (success)
+*/
 
 int main(void)
 {
-puts("\"programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
