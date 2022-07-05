8de308537a30e1e2f34cad32c24d4dc1a2e7cc54
owolabi olatunji <oluwaferanmiadetomiwa7@gmail.com>
2022-07-05 18:00:41 +0100

    nested

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include 'main.h'
+#include <stdio.h>
+
+/**
+ * two_integers - returns the result
+ * Return: void
+ */
+int two_integers(void)
+{
+
+int i = a;
+int j = b;
+
+_putchar("a + b")
+return (0);
+}
